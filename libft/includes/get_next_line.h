a6cc72081535df5dd873744aa0fc105edd7121a0
Dylan Chew <lchew@student.42kl.edu.my>
2023-02-28 13:52:07 +0800

    [Update] Fix norminette error

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -20,9 +20,9 @@
 
 void		ft_free(char **str);
 
-char	*gnl_find_nl(char *buffer, int nbyte, char *txtread, int fd);
-char	*gnl_join(char *s1, char *s2);
-char	*gnl_write(char *buffer, char **res);
+char		*gnl_find_nl(char *buffer, int nbyte, char *txtread, int fd);
+char		*gnl_join(char *s1, char *s2);
+char		*gnl_write(char *buffer, char **res);
 
 char		*get_next_line(int fd);
 
